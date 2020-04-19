78f90217ffcbf1ae54fb62595cc7c28c13848941
Jessica Christine Gray <60930017+jcgray77@users.noreply.github.com>
2020-04-19 17:11:38 -0400

    Add files via upload

---
+++ olympic.js
@@ -0,0 +1,90 @@
+ if(5 > 3){
+    console.log("FIVE IS GREATER THAN THREE")
+ } else if(3 > 5){
+     console.log("THREE IS GREATER THAN FIVE")
+ } else if(3 === 5){
+     console.log("THREE IS EQUAL TO FIVE")
+ } else {
+     console.log("I dont know what's going on")
+ }
+
+if("cat === 3"){
+    console.log("THE LENGTH OF A CAT IS EQUAL TO THREE")
+}  else if("cat > 3"){
+    console.log("CAT IS GREATER THAN THREE")
+}  else if("cat < 3"){
+    console.log("CAT IS LESS THAN THREE")
+}
+
+if("cat is equal to dog"){
+    console.log("NOT THE SAME WHEN THEY ARE NOT EQUAL")
+}
+
+var person = {
+    name: "Bobby",
+    age: 12
+}
+
+if("age < 18 && YOUR NAME STARTS WITH B"){
+    console.log("YOU ARE NOT ALLOWED TO GO TO THE MOVIE")
+}  else if("age > 18 && NAME BEGINS WITH B"){
+    console.log("YOU ARE ALLOWED TO GO TO THE MOVIE")
+}
+
+if("1 === 1"){
+    console.log("STRICT")
+}  else if("1 === 1"){
+    console.log("ABSTRACT")
+}  
+
+if("1 <= 2 && 2 === 4"){
+    console.log("YES")
+}
+
+var pet = "dog"
+if("dog"){
+    console.log("STRING")
+}
+
+var booleanValue = true;
+
+function someFunction(){
+    if(booleanValue){
+        return "something";
+    }
+}
+
+
+
+let boolean = true;
+if(boolean = true){
+    console.log("true")
+}
+
+if (typeof myNewVariable !== 'undefined'){
+    console.log("variable is defined")
+}   else (typeof myNewVariable === 'undifined')
+    console.log("variable is undefined")
+
+myNewVariable = 43;
+if (typeof myNewVariable !== 'undefined'){
+    console.log("variable is defined")
+}   else if (typeof myNewVariable === 'undifined')
+    console.log("variable is undefined")
+
+if ("s" > 12){
+    console.log("s is greater than 12")
+}
+
+if ("z" > 1){
+    console.log("c is greater 21")
+}
+
+if ("g" > 3){
+    console.log("t is greater than 2")
+}
+
+if ("j" > 9){
+    console.log("j is greater than 9")
+}
+
